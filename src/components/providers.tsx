3f96ea16a273315352d05e9e9ec7597d9da8e9e7
n45os <nassosbountioukos@gmail.com>
2026-05-03 23:59:13 +0300

    lets seee

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { SessionProvider } from "next-auth/react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <SessionProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </SessionProvider>
+      <TooltipProvider>{children}</TooltipProvider>
     </ThemeProvider>
   );
 }
